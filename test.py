9d72df2730c47fb14e9d100c47c71e8306ff42e6
Zhexi Liu <zhexil@andrew.cmu.edu>
2018-05-08 14:48:27 -0400

    stash

--- test.py
+++ test.py
@@ -2,6 +2,25 @@ import numpy
 import pandas
 
 # 想砍人系列
+# 5043 x 28
 
 df = pandas.read_csv("movie_metadata.csv")
-print (df.columns)
+df = df.dropna(axis=0, how='any')
+size =  len(df)
+
+def get_genres (dataframe):
+	unique_genres = set([])
+	for row in dataframe['genres']:
+		genres = row.split("|")
+		for g in genres:
+			unique_genres.add(g)
+	unique_genres = sorted(unique_genres)
+	return unique_genres
+
+
+unique_genres = get_genres (df)
+
+
+for genre in unique_genres:
+	genre_idx = df[df['genres'].str.contains(genre)].index
+	
