6dfab29297f9aa0d9733a4736b5266bfcb42c500
rongpenz <rongpenz@andrew.cmu.edu>
2018-05-11 16:00:05 -0400

    rp

--- test2.py
+++ test2.py
@@ -1,18 +1,55 @@
-import numpy as np
+import json
+import glob
+import sklearn
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.utils import shuffle
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import normalize
 
-from collections import Counter
-# 5043 x 28
+def get_log_label (bins, box_office_value):
+    log_val = np.log(box_office_value)
+    for i in range(len(bins[1])):
+        if bins[1][i] <= log_val and log_val <= bins[1][i+1]:
+            return i
+    return len(bins[1]) - 1
 
 
 def get_labels(gross):
-    gross_classes = np.array([0, 0.1,0.2,0.3,0.4,0.5,0.6,0.8,1.0,1.2,1.4,1.7,2.0,2.4,2.8,3.2,3.6,4.0,4.5,5.0,5.5,6.0,6.5,7.0,float('Inf')]) * 1e8
+    
+    bin_size = 3
+    bins = plt.hist(np.log(gross), bin_size)
+
+    #for i in range(bin_size):
+    #    print ("label", i+1 , ":",  np.exp(bins[1][i]), '-' ,np.exp(bins[1][i+1]), "," , str(bins[0][i]) , "movies")
+        
+    
+    #gross_classes = np.array([0,0.5,1.0,1.2,1.4,1.7,2.0,2.4,2.8,3.2,3.6,4.0,4.5,5.0,5.5,6.0,6.5,7.0,float('Inf')]) * 1e8
+    labels = []
+    for i in gross:
+        labels.append(get_log_label(bins, i))
+    
+    return labels
+    
+    '''
+    labels = []
+    for i in gross:
+        for j in range(len(gross_classes)):
+            if gross_classes[j] <= i < gross_classes[j+1]:
+                labels.append(j)
+    return labels
+    '''
+
+
+'''
+def get_labels(gross):
+    gross_classes = np.array([0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08, 0.09, float('Inf')]) * 1e8
     
     labels = []
     for i in gross:
@@ -20,162 +57,129 @@ def get_labels(gross):
             if gross_classes[j] <= i < gross_classes[j+1]:
                 labels.append(j)
     return labels
-
-def remove_column(df, drop_list):
-	for col_name in drop_list:
-		df.drop(col_name, axis=1, inplace=True)
-	return df
-
-def get_genres (dataframe):
-	unique_genres = set([])
-	for row in dataframe['genres']:
-		genres = row.split("|")
-		for g in genres:
-			unique_genres.add(g)
-	unique_genres = sorted(unique_genres)
-	return unique_genres
-
-def one_hot_encode_genres (dataframe):
-	size =  len(dataframe)
-	unique_genres = get_genres (df)
-	for genre in unique_genres:
-		genre_idx = df[df['genres'].str.contains(genre)].index
-		arr = [0] * size
-		for i in genre_idx:
-			arr[i] = 1
-		df[genre] = arr
-	return df
-
-def one_hot_encode(dataframe, column):
-	size =  len(dataframe)
-	unique_genres = np.unique(df[column])
-	for genre in unique_genres:
-		genre_idx = df[df[column].str.match(genre)].index
-		arr = [0] * size
-		for i in genre_idx:
-			arr[i] = 1
-		df[genre] = arr
-	return df
-
-def analysis(y_p, y_t):
-    diff = y_p - y_t
-    
-    for i,val in enumerate(diff):
-        if val != 0:
-            print(y_p[i], y_t[i], val, gross_df[3500+i]) 
-            
-def wrong_pred_distribution(y_p, y_t):
-    diff = y_p - y_t
-    c = Counter()
-    for i,val in enumerate(diff):
-        if val != 0:
-            c[y_p[i]] += 1
-    return c
-
-def wrong_true_distribution(y_p, y_t):
-    diff = y_p - y_t
-    c = Counter()
-    for i,val in enumerate(diff):
-        if val != 0:
-            c[y_t[i]] += 1
-    return c
-
-            
-def get_wrong_index(y_p, y_t):
-    diff = y_p - y_t
-    index = []
-    for i,val in enumerate(diff):
-        if val != 0:
-            index.append(i)
-    return index
-
-df = pd.read_csv("movie_metadata.csv")
-# define this list for the unwanted columns to drop
-drop_columns = ["movie_imdb_link", "plot_keywords", "language"]
-df = remove_column(df, drop_columns)
-
-df['movie_title'] = df['movie_title'].str.strip()
-df = df.dropna(axis=0, how='any')
-df = df.reset_index()
-
 '''
+def one_hot_encoding_column(col_name):
+	item_map = {}
+	item_count = 0
+	for i in range(movie_size):
+		items = str(df[col_name].iloc[i])
+		items_arr = items.split(",")
+		for item in items_arr:
+			item = item.strip()
+			if item not in item_map:
+				item_map[item] = item_count
+				item_count += 1
+
+	Matrix = [[0 for x in range(item_count)] for y in range(movie_size)] 
+	for i in range(movie_size):
+		items = str(df[col_name].iloc[i])
+		items_arr = items.split(",")
+		for item in items_arr:
+			item = item.strip()
+			idx = item_map[item]
+
+			Matrix[i][idx] = 1
+	return item_map, np.asarray(Matrix)
+
+def recover_key(dicty,value):
+    for a_key in dicty.keys():
+        if (dicty[a_key] == value):
+            return a_key
+
+
+
+df = pd.read_csv('result.csv',index_col=['imdbID'], header=0)
+df = df[np.isfinite(df['Opening Weekend USA'])]
+
+
 df1 = pd.read_csv("google_index.csv")
 sum_index = df1.sum(axis=1)
 df1 = pd.concat([df1['title'],sum_index],axis=1, join_axes=[df1.index]).rename(index=str, columns={0: "google_index"})
-df = df.merge(right=df1, left_on="movie_title", right_on="title")
-'''
+df = df.merge(right=df1, left_on="Title", right_on="title")
 
-gross_df = df['gross']
-
-y = get_labels(df['gross'])
-
-df = one_hot_encode_genres(df)
-#csv_1 = df.to_csv("movie_genre.csv") #export
-df = one_hot_encode(df, 'content_rating')
-df = one_hot_encode(df, 'country')
-df = remove_column(df, ["movie_title",'director_facebook_likes', 'actor_3_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes',
-'facenumber_in_poster', 'actor_2_facebook_likes', 'movie_facebook_likes','aspect_ratio',
-        'index', 'color','genres','gross', 'content_rating', 'country','actor_1_name','actor_2_name','actor_3_name','director_name'])
-    
-X = df.as_matrix()
-y = np.array(y)
-
-#################
-clf_lg = LogisticRegression()
-clf_lg.fit(X[:3500,:], y[:3500])
-y_p = clf_lg.predict(X[3500:,:])
-print("Validation accuracy:", np.mean(y_p==y[3500:]))
-
-#analysis(y_p, y[3500:])
-print(wrong_pred_distribution(y_p, y[3500:]))
-print(wrong_true_distribution(y_p, y[3500:]))
+df = shuffle(df)
 
 
-#################
-clf = SVC()
-clf.fit(X[:3500,:], y[:3500])
-y_p = clf.predict(X[3500:,:])
-'''
-for i, val in enumerate(y_p):
-    if val == 0:
-        y_p[i] = clf_lg.predict(X[i,:].reshape(1, -1))
-'''
-print("Validation accuracy:", np.mean(y_p==y[3500:]))
+movie_size = (len(df))
+test_size = int(movie_size/8)
+sample_size = movie_size - test_size
 
-print(wrong_pred_distribution(y_p, y[3500:]))
-print(wrong_true_distribution(y_p, y[3500:]))
-indice = get_wrong_index(y_p, y[3500:])
-df.iloc[indice,:].to_csv("wrong.csv")
-print(y_p)
-#################
-clf =  MLPClassifier(alpha=0.3,activation='logistic')
-clf.fit(X[:3500,:], y[:3500])
-y_p = clf.predict(X[3500:,:])
-print("Validation accuracy:", np.mean(y_p==y[3500:]))
-
-print(wrong_pred_distribution(y_p, y[3500:]))
-print(wrong_true_distribution(y_p, y[3500:]))
-
-#################
-clf = KNeighborsClassifier(7)
-clf.fit(X[:3500,:], y[:3500])
-y_p = clf.predict(X[3500:,:])
-print("Validation accuracy:", np.mean(y_p==y[3500:]))
-
-print(wrong_pred_distribution(y_p, y[3500:]))
-print(wrong_true_distribution(y_p, y[3500:]))
+df['BoxOffice'] = df['BoxOffice'].replace( '[\$,)]','', regex=True ).astype(float)
+df['Runtime'] = df['Runtime'].replace( 'min','', regex=True ).astype(int)
 
 
-#################
-clf_tree = DecisionTreeClassifier(max_depth=5)
-clf_tree.fit(X[:3500,:], y[:3500])
-y_p = clf_tree.predict(X[3500:,:])
-print("Validation accuracy:", np.mean(y_p==y[3500:]))
-
-print(wrong_pred_distribution(y_p, y[3500:]))
-print(wrong_true_distribution(y_p, y[3500:]))
+df['imdbRating'] = df['imdbRating'].round()
+labels = get_labels(df['Opening Weekend USA'])
 
 
-for i, val in enumerate(clf_tree.feature_importances_):
-    if val != 0:
-        print(df.columns[i],'\t', val)
+actors_map, actors_Matrix = one_hot_encoding_column('Actors')
+genre_map, genre_Matrix = one_hot_encoding_column('Genre')
+contry_map, country_Matrix = one_hot_encoding_column('Country')
+production_map, production_Matrix = one_hot_encoding_column('Production')
+rated_map, rated_Matrix = one_hot_encoding_column('Rated')
+year_map, year_Matrix = one_hot_encoding_column('Year')
+director_map, director_Matrix = one_hot_encoding_column('Director')
+google_index_Matrix = normalize(df.loc[:, ['google_index']].as_matrix(), axis=0) 
+
+#year_Matrix = df['Year'].as_matrix()
+#print(year_Matrix.shape)
+#runtime_matrix = df['Runtime'].as_matrix() 
+#runtime_matrix = df.loc[:, ['Year', 'Runtime']].as_matrix() 
+
+
+t_Matrix = np.concatenate((actors_Matrix, \
+							production_Matrix,\
+							genre_Matrix, \
+							country_Matrix, \
+							rated_Matrix,\
+							year_Matrix,\
+                                google_index_Matrix
+							), axis=1)
+
+
+f_Matrix = actors_Matrix
+
+print (t_Matrix.shape, t_Matrix.shape )
+
+x_train = t_Matrix[:sample_size]
+x_test = t_Matrix[sample_size: ]
+
+y_train = np.array(labels[:sample_size])
+y_test = np.array(labels[sample_size: ])
+
+print ("Finished Setting up testing and training set:")
+print (x_train.shape, y_train.shape)
+
+#clf_l2_LR = DecisionTreeClassifier()
+
+clf_l2_LR = LogisticRegression(multi_class="multinomial", solver = 'lbfgs')
+
+
+
+clf_l2_LR.fit(x_train, y_train)
+y_predit = clf_l2_LR.predict(x_test)
+
+"""
+importances = clf_l2_LR.feature_importances_
+indices = np.argsort(importances)[::-1]
+# Print the feature ranking
+
+
+
+print("Feature ranking:")
+
+cc = 0
+for f in range(t_Matrix.shape[1]):
+    print("%d. %s (%f)" % (f + 1, recover_key(director_map, indices[f]), importances[indices[f]]))
+    if cc >=20:
+    	break
+    cc += 1
+
+
+"""
+
+#for ii in range (len(y_test)):
+	#print (y_predit[ii], y_test[ii])
+
+
+print (np.mean(y_predit == y_test))
